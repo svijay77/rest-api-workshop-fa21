284da2503129d17c657e573fe5f2d2ec3bd55987
Max Fan <root@max.fan>
2021-09-23 19:06:46 -0500

    Enable CORS

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 events = {
         0 : {
@@ -22,6 +23,7 @@ events = {
         }}
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 @app.get("/all")
